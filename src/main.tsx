81e4aa5a6f026bea41cf502171b6f04bf23447ad
aryanbloch <aryanbloch53@gmail.com>
2026-09-26 04:14:13 +0530

    Mount sonner Toaster so checkout success/error toasts render

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
+import { Toaster } from "sonner";
 import "./index.css";
 import Index from "./pages/Index.tsx";
 
-createRoot(document.getElementById("root")!).render(<Index />);
+createRoot(document.getElementById("root")!).render(
+  <>
+    <Index />
+    <Toaster position="top-center" richColors />
+  </>,
+);
