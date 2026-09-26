1ba53248383fc572a64cce5f5f211067d32fbda0
boldandbrilliantnailstudio <boldandbrilliantnailstudio@gmail.com>
2026-09-26 17:01:11 +0530

    Wrap app in SiteSettingsProvider

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import Index from "./pages/Index.tsx";
 import AdminPage from "./pages/admin.tsx";
 import { CartProvider } from "./hooks/use-cart.tsx";
+import { SiteSettingsProvider } from "./hooks/use-site-settings.tsx";
 import ProfileProvider from "./components/profile/profile-provider.tsx";
 
 // /admin shows a separate password-gated dashboard for the studio owner - see src/pages/admin.tsx.
@@ -35,11 +36,13 @@ createRoot(document.getElementById("root")!).render(
       <Toaster position="top-center" richColors />
     </>
   ) : (
-    <CartProvider>
-      <ProfileProvider>
-        <Index />
-      </ProfileProvider>
-      <Toaster position="top-center" richColors />
-    </CartProvider>
+    <SiteSettingsProvider>
+      <CartProvider>
+        <ProfileProvider>
+          <Index />
+        </ProfileProvider>
+        <Toaster position="top-center" richColors />
+      </CartProvider>
+    </SiteSettingsProvider>
   ),
 );
